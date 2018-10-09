97dd9ede308f8ca571580e886321b5ae7530746c
WeebySqueezy <43782162+WeebySqueezy@users.noreply.github.com>
2018-10-09 12:12:36 -0400

    With dropdowns

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-    
+  $
+    function saveA {
+localStorage.setItem("1.1","1.1".value);localStorage.setItem("1.2","1.2".value);localStorage.setItem("1.3","1.3".value);localStorage.setItem("1.3","1.3".value); 
+    }
+    function loadA {
+        document.getElementById("A").innerHTML = localStorage.getItem("1.1","1.2"."1.3","1.4");
+    }
+window.onClose = saveA;
+window.onLoad = loadA;
 
 });
