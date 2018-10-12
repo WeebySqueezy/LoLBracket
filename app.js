8e2e80ccc8ac11df3d0227dc0b6a6f3e59bcdbc3
WeebySqueezy <43782162+WeebySqueezy@users.noreply.github.com>
2018-10-12 08:25:48 -0400

    With data array

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
 $(document).ready(function() {
-  $
-    function saveA {
-localStorage.setItem("1.1","1.1".value);localStorage.setItem("1.2","1.2".value);localStorage.setItem("1.3","1.3".value);localStorage.setItem("1.3","1.3".value); 
-    }
-    function loadA {
-        document.getElementById("A").innerHTML = localStorage.getItem("1.1","1.2"."1.3","1.4");
-    }
-window.onClose = saveA;
-window.onLoad = loadA;
 
+   $("#oneone").change(function() {
+       var txt = $("#oneone").val();
+       console.log(txt);
+       saveA();
+       
+   });
+    $("#onetwo").change(function() {
+       var txt = $("#onetwo").val();
+        console.log(txt);
+    }); $("#twoone").change(function() {
+       var txt = $("#twoone").val();
+        console.log(txt);
+    });
+    
+    var data = {
+    A: {
+      team1: $("#oneone").val(),
+      team2: $("#onetwo").val(),
+      team3: $("#onethree").val(),
+      team4: $("#onefour").val()
+    },
+    B: {
+        team1: $("#twoone").val(),
+        team2: $("#twotwo").val(),
+        team3: $("#twothree").val(),
+        team4: $("#twofour").val()
+     },
+    C: {
+        team1: $("#threeone").val(),
+        team2: $("#threetwo").val(),
+        team3: $("#threethree").val(),
+        team4: $("#threefour").val()
+        },
+    D: {
+        team1: $("#fourone").val(),
+        team2: $("#fourtwo").val(),
+        team3: $("#fourthree").val(),
+        team4: $("#fourfour").val(),
+    }
+    };
+   
+    function saveA() {
+        localStorage.setItem("#oneone",A[1]);
+    }
+    function loadA() {
+        document.getElementById("oneone").innerHTML = localStorage.getItem(A[1]);
+        
+    }
 });
+   
+    
+ 
